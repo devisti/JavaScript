5c5d4c0480c815c7746ee7a5835d26b24c79b504
Isti Hoque <115354997+devisti@users.noreply.github.com>
2025-05-26 16:09:43 +0530

    Update quick_quiz.js

--- Chapter 1/quick_quiz.js
+++ Chapter 1/quick_quiz.js
@@ -1,2 +1,13 @@
 // write javascript program to store name, phone number and marks of a student using objects.
 
+const student = {
+    name : "Shahid Raza",
+    phone : 7484930484,
+    marks : "350 out of 500",
+
+};
+
+
+
+
+console.log(student);
