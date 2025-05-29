fc445f6e2407c47b34089f0e68a37ffd92be0e04
Isti Hoque <115354997+devisti@users.noreply.github.com>
2025-05-29 16:27:26 +0530

    Create practice_set.js

---
+++ Chapter 2/practice_set.js
@@ -0,0 +1,11 @@
+// use logical operator to find whether the age of a person lies between 10 and 20 ?
+
+person = 14;
+
+
+if (person >= 10 && person <= 20) {
+    console.log("Yes, the person lies between age 10 and 20.")
+}
+else{
+    console.log("No, the person doesn't lies between age 10 and 20")
+}
